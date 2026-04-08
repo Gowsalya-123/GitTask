5aee2c0053b388ec8284be0c71b0501b247b527b
Gowsalya-123 <kousikasuresh98@gmail.com>
2026-04-08 17:37:23 +0530

    Added methods from Sharmi

--- src/test/java/com/gittask/ClassA.java
+++ src/test/java/com/gittask/ClassA.java
@@ -2,19 +2,32 @@ package com.gittask;
 
 public class ClassA {
 
-		private void method1() {
-			System.out.println("1");
-
-		}
-		private void method2() {
-			System.out.println("2");
-
-		}
-		private void method3() {
-			System.out.println("3");
-
-		}
+	private void method1() {
+		System.out.println("1");
 
 	}
 
+	private void method2() {
+		System.out.println("2");
 
+	}
+
+	private void method3() {
+		System.out.println("3");
+
+	}
+
+	private void tamilnadu() {
+		System.out.println("TAMIL");
+
+	}
+
+	private void kerala() {
+		System.out.println("MALAYALAM");
+	}
+
+	private void andhra() {
+		System.out.println("TELUGU");
+	}
+
+}
